b71794d991624bd5751247394e705a9e5a28cb32
Shveta Puri <shvetapuri@gmail.com>
2013-06-03 14:20:12 -0700

    with favorite feature

---
+++ iReflect/iReflectMainPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  iReflectMainPageViewController.h
+//  iReflect
+//
+//  Created by Shveta Puri on 5/21/13.
+//  Copyright (c) 2013 Shveta Puri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iReflectMainPageViewController : UIViewController
+
+@end
---
+++ iReflect/iReflectMainPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  iReflectMainPageViewController.m
+//  iReflect
+//
+//  Created by Shveta Puri on 5/21/13.
+//  Copyright (c) 2013 Shveta Puri. All rights reserved.
+//
+
+#import "iReflectMainPageViewController.h"
+
+@interface iReflectMainPageViewController ()
+
+@end
+
+@implementation iReflectMainPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
